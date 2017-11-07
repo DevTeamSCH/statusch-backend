9b8c2b6a5a777b207d1d930488712d5a291d1157
Lénárt Levente <fabledpig@gmail.com>
2017-11-07 20:57:55 +0100

    Build in algorithm

--- src/laundry_room/apps.py
+++ src/laundry_room/apps.py
@@ -1,5 +1,22 @@
+import fcntl
+import atexit
 from django.apps import AppConfig
-
+from laundry_room.listener import Listener
 
 class LaundryRoomConfig(AppConfig):
     name = 'laundry_room'
+
+    def __init__(self, app_name, app_module):
+        AppConfig.__init__(self, app_name, app_module)
+        self._lock_file = None
+
+    def ready(self):
+        print('New app process')
+        self._lock_file = open('./lock', 'w')
+        try:
+            fcntl.flock(self._lock_file, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            listener = Listener()
+            listener.start()
+            atexit.register(listener.stop)
+        except BlockingIOError:
+            pass
--- src/laundry_room/models.py
+++ src/laundry_room/models.py
@@ -13,7 +13,7 @@ class Machine(models.Model):
         (2, "N/A"),
     )
 
-    kind_of = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    type = models.CharField(max_length=2, choices=TYPE_CHOICES)
     status = models.IntegerField(blank=True, choices=STATUS_CHOICES, null=True)
     message = models.CharField(blank=True, null=True, max_length=255)
     floor = models.ForeignKey(Floor, related_name="machines", on_delete=models.DO_NOTHING)
