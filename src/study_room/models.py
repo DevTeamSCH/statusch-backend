f52c590ffde54b8ba293d4024d86f7897ede6c19
Levente Lénárt <fabledpig@gmail.com>
2018-01-07 17:18:19 +0100

    study_room: Change status attribute's type to IntegerField

--- src/study_room/models.py
+++ src/study_room/models.py
@@ -3,8 +3,14 @@ from common.models import Floor
 
 
 class StudyRoom(models.Model):
+    STATUS_CHOICES = (
+        (0, "Free"),
+        (1, "Busy"),
+        (2, "N/A"),
+    )
+
     name = models.CharField(max_length=255)
-    status = models.BooleanField()
+    status = models.IntegerField(blank=True, choices=STATUS_CHOICES, null=True)
     floor = models.ForeignKey(Floor, related_name="study_rooms")
 
     def __str__(self):
