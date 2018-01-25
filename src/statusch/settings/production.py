1b4bc4431998597121e6ef3e83cd75f8e014e0cd
Barnabás Czémán <trabarni@gmail.com>
2018-01-25 16:32:25 +0100

    Add postgres suppport for production

--- src/statusch/settings/production.py
+++ src/statusch/settings/production.py
@@ -1,3 +1,15 @@
+import os
 from local import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'statusch'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
