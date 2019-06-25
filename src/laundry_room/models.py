ee012abed267a32a79509693557b5634b3ea613f
Bodor Máté <bmate711@gmil.com>
2019-06-25 23:01:21 +0200

    Add machine type

--- src/laundry_room/models.py
+++ src/laundry_room/models.py
@@ -2,19 +2,28 @@ from django.db import models
 from common.models import Floor
 
 
-class Machine(models.Model):
+class Type(models.Model):
     TYPE_CHOICES = (("WM", "WashingMachine"), ("DR", "Dryer"))
-    STATUS_CHOICES = ((0, "Free"), (1, "Busy"), (2, "N/A"))
 
     kind_of = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    name = models.CharField(max_length = 125)
+    treshhold = models.IntegerField()
+
+
+class Machine(models.Model):
+    STATUS_CHOICES = ((0, "Free"), (1, "Busy"), (2, "N/A"))
+
     status = models.IntegerField(blank=True, choices=STATUS_CHOICES, null=True)
     message = models.CharField(blank=True, null=True, max_length=255)
     floor = models.ForeignKey(
         Floor, related_name="machines", on_delete=models.DO_NOTHING
     )
+    type = models.ForeignKey(
+        Type, related_name="machines", on_delete=models.DO_NOTHING
+    )
 
     def __str__(self):
-        return "{} {}".format(self.floor, self.kind_of)
+        return "{} {}".format(self.floor, self.type)
 
 
 class Value(models.Model):
