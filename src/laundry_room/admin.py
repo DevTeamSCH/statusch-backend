273b2066b4e8b12868e506bcb0e135f2fc8fefd0
Bodor Máté <bmate711@gmil.com>
2019-07-20 18:13:35 +0200

    Register type to admin page

--- src/laundry_room/admin.py
+++ src/laundry_room/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Machine
+from laundry_room.models import Machine
+from laundry_room.models import Type
 
 
 admin.site.register(Machine)
+admin.site.register(Type)
